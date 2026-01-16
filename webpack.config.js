8b8af31d863603b53e53f3c042a02c82fdd45629
reenaverma <reena_verma@hotmail.com>
2026-01-16 14:22:19 +0000

    Update webpack for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'development',
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
